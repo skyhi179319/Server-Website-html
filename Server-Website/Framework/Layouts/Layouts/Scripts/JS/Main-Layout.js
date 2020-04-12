93186b9ec6cb12f3653b0421e454cd8130ba7bdf
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2020-04-12 10:24:42 -0500

    Updated Layouts Folder

---
+++ Server-Website/Framework/Layouts/Layouts/Scripts/JS/Main-Layout.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$("#ImportNav").load("http://127.0.0.1/Server-Website/Framework/Layouts/Navbar.html");
+	$("#ImportHeader").load("http://127.0.0.1/Server-Website/Framework/Layouts/Header.html");
+}); 
