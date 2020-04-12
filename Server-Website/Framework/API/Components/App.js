045548ed82ae1c8a7ddb6ce9252bb56201a979fd
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2020-04-12 11:38:28 -0500

    Updated API Folder

---
+++ Server-Website/Framework/API/Components/App.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$("#ImportApp").load("http://127.0.0.1/Server-Website/Framework/API/App/Layouts/Program/Layout.html");
+}); 
---
+++ Server-Website/Framework/API/Improvements/Side-Menu/Full/Page.js
@@ -0,0 +1,1 @@
+$("#Details").load("http://127.0.0.1/Server-Website/Framework/API/Improvements/Details.html");
