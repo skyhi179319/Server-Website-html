d7ca7da93149baff9661a0683ff4dbf0b8b433df
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2020-04-16 18:22:24 -0500

    Delete MainLayout.js

--- Server-Website/Framework/Layouts/Scripts/JS/MainLayout.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	$("#ImportNav").load("http://127.0.0.1/Server-Website/Framework/Layouts/NavBar.html");
-	$("#ImportHeader").load("http://127.0.0.1/Server-Website/Framework/Layouts/Header.html");
-}); 
